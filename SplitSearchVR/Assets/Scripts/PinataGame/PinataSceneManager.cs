062b5bdbbc23683e85622e663736cc7827772e4d
toddlittle <todd@fairworlds.com>
2019-06-22 16:59:44 -0700

    added Pendulum game

---
+++ SplitSearchVR/Assets/Scripts/PinataGame/PinataSceneManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinataSceneManager : MicroScene
+{
+    public void OnSuccess()
+    {
+        GameManager.Instance.OnGameSuccess();
+    }
+
+
+}
