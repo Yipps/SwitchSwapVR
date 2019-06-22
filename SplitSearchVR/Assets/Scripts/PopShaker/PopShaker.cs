83a8cf0672d8a413e847668ec6b6649a2d1d2266
patrickowens <pat.owens@gmail.com>
2019-06-22 18:21:12 -0500

    Added scripts to see if the player is shaking the can and if they meet the win conditions (i.e., shaking the can enough before the time limit)

--- SplitSearchVR/Assets/Scripts/PopShaker/PopShaker.cs
+++ SplitSearchVR/Assets/Scripts/PopShaker/PopShaker.cs
@@ -2,26 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PopShaker : MonoBehaviour
+public class PopShaker : MicroScene
 {
-    public Transform _Hand;
 
-    public Vector3 deltaHand;
-    public Vector3 LastHandPosition;
-    public float totalHandMovement;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
 
     // Update is called once per frame
     void Update()
     {
-        deltaHand = LastHandPosition - _Hand.transform.localPosition;
-        totalHandMovement += deltaHand.magnitude;
 
-        LastHandPosition = _Hand.transform.localPosition;
     }
+
+    public void StartTimer()
+    {
+
+    }
+
+
+
+
 }
