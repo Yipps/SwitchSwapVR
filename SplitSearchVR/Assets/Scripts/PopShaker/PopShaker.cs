bb07bba98ccb1168d9becd53df15f71a9e6723b2
patrickowens <pat.owens@gmail.com>
2019-06-22 17:03:19 -0500

    Adding popshaker level (meant to commit together with last commit)

---
+++ SplitSearchVR/Assets/Scripts/PopShaker/PopShaker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopShaker : MonoBehaviour
+{
+    public Transform _Hand;
+
+    public Vector3 deltaHand;
+    public Vector3 LastHandPosition;
+    public float totalHandMovement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        deltaHand = LastHandPosition - _Hand.transform.localPosition;
+        totalHandMovement += deltaHand.magnitude;
+
+        LastHandPosition = _Hand.transform.localPosition;
+    }
+}
