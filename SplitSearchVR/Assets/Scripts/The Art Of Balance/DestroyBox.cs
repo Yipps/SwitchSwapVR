ef76e8c142cbaf8e5775cd0bc952b95b45ce4d25
DESKTOP-565DQHM\diego <diego.calvin.12@gmail.com>
2019-06-22 22:01:03 -0700

    Added sounds/New game

---
+++ SplitSearchVR/Assets/Scripts/The Art Of Balance/DestroyBox.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBox : MonoBehaviour
+{
+    // Start is called before the first frame update
+	public BalanceMicroManager cmm;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col){
+    	if(col.tag == "Box"){
+    		Debug.Log("Whoops");
+    		cmm.OnBoxTouchedGround();
+    		Destroy(col.gameObject);
+    	}
+    }
+}
