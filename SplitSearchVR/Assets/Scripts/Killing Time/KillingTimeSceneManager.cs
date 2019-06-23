c5036363484d676052bc4de7815c51824a2378d5
Cherison <dcherison@gmail.com>
2019-06-23 03:46:37 +0300

    Killing Time - Test

---
+++ SplitSearchVR/Assets/Scripts/Killing Time/KillingTimeSceneManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillingTimeSceneManager : MicroScene
+   
+
+{
+    public Transform target;
+    private Vector3 targetposition;
+
+
+    public void RandomTargetPlacement()
+    {
+
+        targetposition = UnityEngine.Random.onUnitSphere * 2;
+        targetposition.y = 1;
+        target.position = targetposition;
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+        Debug.DrawRay(transform.position, fwd * 10);
+        if (OVRInput.Get(OVRInput.RawButton.RIndexTrigger))
+        {
+            if (Physics.Raycast(transform.position, fwd, 3))
+            {
+                print("There is something in front of the object!");
+                GameManager.Instance.OnGameSuccess();
+                target.gameObject.SetActive (false);
+            }
+                
+
+        }
+        
+    }
+}
